eb21a76776646d62baad2f3e50f1ce7fe898b122
Aji Ninan <ajininan72@gmail.com>
2022-10-13 17:52:46 +0530

    basic_designing

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div>
+      <div className="banner">
+        <div className="content">
+          <h1 className="title">Movie Name</h1>
+          <div className="banner_buttons">
+            <button className="button">Play</button>
+            <button className="button">My list</button>
+          </div>
+          <h1 className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sunt ut
+            ratione pariatur non
+          </h1>
+        </div>
+        <div className="fade_bottom"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
